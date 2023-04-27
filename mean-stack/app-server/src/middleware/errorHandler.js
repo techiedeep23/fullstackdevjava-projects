129e0e9b021c9a6fa37b2da4fd68a004e51443ae
techiedeep <62819643+techiedeep@users.noreply.github.com>
2023-04-27 09:10:06 -0700

    Update errorHandler.js

--- mean-stack/app-server/src/middleware/errorHandler.js
+++ mean-stack/app-server/src/middleware/errorHandler.js
@@ -1,34 +1,34 @@
-const { constants } = require("../constants");
+const httpStatus = require('http-status-codes');
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
+  const statusCode = res.statusCode ? res.statusCode : httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
 
   switch (statusCode) {
-    case constants.BAD_REQUEST:
+    case httpStatus.StatusCodes.BAD_REQUEST:
       res.json({
         title: "The server cannot or will not process the request due to something that is perceived to be a client error",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case constants.NOT_FOUND:
+    case httpStatus.StatusCodes.NOT_FOUND:
       res.json({
         title: "Requested resource is not available",
         message: err.message,
         stackTrace: err.stack,
       });
-    case constants.UNAUTHORIZED:
+    case httpStatus.StatusCodes.UNAUTHORIZED:
       res.json({
         title: "Unauthorized , no or invalid credentials",
         message: err.message,
         stackTrace: err.stack,
       });
-    case constants.FORBIDDEN:
+    case httpStatus.StatusCodes.FORBIDDEN:
       res.json({
         title: "The server understands the request but refuses to authorize it",
         message: err.message,
         stackTrace: err.stack,
       });
-    case constants.SERVER_ERROR:
+    case httpStatus.StatusCodes.INTERNAL_SERVER_ERROR:
       res.json({
         title: "The server encountered an unexpected condition that prevented it from fulfilling the request",
         message: err.message,
